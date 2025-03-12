383e3df51814341130dfaa3877d6935cf74c6def
chriswilder3 <chriswilderdelta19@gmail.com>
2025-03-12 21:04:15 +0530

    add story req from frontend to backend. Sucess.

--- frontend/reactmain/src/components/index/content/Content.jsx
+++ frontend/reactmain/src/components/index/content/Content.jsx
@@ -1,13 +1,28 @@
 import React, { useRef, useState } from 'react'
 
+
 function Content() {
     const [promptResp, setPromptResp] = useState('')
     const promptInputRef = useRef('null')
-    const handlePrompt = () =>{
+
+    const handlePrompt = async () =>{
         let promptMsg = promptInputRef.current.value
-        console.log(promptMsg);
-        let resp = promptMsg
-        setPromptResp(resp)
+        // console.log(promptMsg);
+
+        const base_url = 'http://localhost:5000'
+        try{
+          const resp = await fetch(`${base_url}/generate-story`,{
+              method : "POST",
+              headers : {"Content-Type":"application/json"},
+              body : JSON.stringify({ "prompt" : promptMsg})
+          })
+          const data = await resp.json()
+          setPromptResp(data.story)
+        }
+        catch(err){
+            console.error("Error:", error);
+            return "Failed to generate story.";
+        }
     }
     return (
       <div className="max-w-2xl mx-auto mt-10 p-8 bg-gray-800 border border-gray-700 rounded-xl shadow-lg text-gray-100">
